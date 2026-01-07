25f9801c09c9567937d6702f4c433e6df10e0273
Sheldon Monteiro <sheldonmonteiro10@gmail.com>
2026-01-07 21:09:18 +0530

    Commit 3, made changes in the game, could not add the word meaning part, so made improvements to the game.

--- Day3_If_else_game.py
+++ Day3_If_else_game.py
@@ -1,22 +1,44 @@
 from random_word import RandomWords
-r = RandomWords()
 
-word = r.get_random_word()
-guessed = ["_" for _ in word]
-attempts = 6
+def play_game():
+    r = RandomWords()
+    word = r.get_random_word()
+    guessed = ["_" for _ in word]
+    attempts = 6
+    guessed_letters = set()
 
-while attempts > 0 and "_" in guessed:
-    print("Word: ", " ".join(guessed))
-    guess = input("Guess a letter: ")
-    if guess in word:
-        for i in range(len(word)):
-            if word[i] == guess:
-                guessed[i] = guess
+    while attempts > 0 and "_" in guessed:
+        print("\nWord: ", " ".join(guessed))
+        print("Missed letters: ", " ".join([g for g in guessed_letters if g not in word]))
+
+        guess = input("Guess a letter: ").lower()
+
+        if not guess.isalpha() or len(guess) != 1:
+            print("Please enter a single letter.")
+            continue
+
+        if guess in guessed_letters:
+            print("You already guessed that letter!")
+            continue
+
+        guessed_letters.add(guess)
+
+        if guess in word:
+            for i in range(len(word)):
+                if word[i] == guess:
+                    guessed[i] = guess
+        else:
+            attempts -= 1
+            print(f"Incorrect! {attempts} attempts left.")
+
+    if "_" not in guessed:
+        print(f"\n🎉 Congrats! You found the word: {word}")
     else:
-        attempts -= 1
-        print(f"Incorrect! {attempts} attempts left.")
+        print(f"\n💀 Game over! The word was: {word}")
 
-if "_" not in guessed:
-    print("Congrats! You found the word:", word)
-else:
-    print("Game over! The word was:", word)
+while True:
+    play_game()
+    play_again = input("\nDo you want to play again? (y/n): ").lower()
+    if play_again != "y":
+        print("Thanks for playing! Goodbye 👋")
+        break
